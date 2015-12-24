8eab998646d170cc53b316095212020ec4ca785e
tanayuth <yuth@abctech-thailand.com>
2015-12-24 12:59:19 +0700

    create image and config table

---
+++ src/main/java/com/abc/dkadmin/DockerMyAdminApplication.java
@@ -0,0 +1,12 @@
+package com.abc.dkadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerMyAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerMyAdminApplication.class, args);
+    }
+}
