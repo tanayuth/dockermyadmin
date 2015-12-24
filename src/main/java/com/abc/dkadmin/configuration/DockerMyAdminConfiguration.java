e166c26358ab187aed59ab27fdac83cf432b51c7
kchiwa <pea@abctech-thailand.com>
2015-12-24 14:57:04 +0700

    Add configuration bean

---
+++ src/main/java/com/abc/dkadmin/configuration/DockerMyAdminConfiguration.java
@@ -0,0 +1,22 @@
+package com.abc.dkadmin.configuration;
+
+import com.abc.dkadmin.properties.DockerMyAdminProperties;
+import com.abc.dkadmin.service.UnixProcessor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DockerMyAdminConfiguration {
+
+    @Bean
+    public DockerMyAdminProperties dockerMyAdminProperties() {
+        return new DockerMyAdminProperties();
+    }
+
+    @Bean
+    public UnixProcessor unixProcessor() {
+        return new UnixProcessor();
+    }
+}
+
+
